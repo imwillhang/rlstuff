9dcf994e14226647804f0ef87394ce1c8b324ddb
William Hang <williamhang97@gmail.com>
2018-02-10 16:13:14 -0800

    new commit

--- practicea3c.py
+++ practicea3c.py
@@ -1,26 +1,31 @@
 # A worker in A3C
 # Parameters are broadcast to the worker and periodically updated
 
+import tensorflow as tf
+
+import tensorflow.contrib.layers as layers
 import tensorflow.contrib.slim as slim
+
 import numpy as np
+from preprocess import greyscale as processFrame
 
-from tensorflow import nn, layers, keras, losses
+from tensorflow import nn, keras, losses
 
-# TODO:
-#	make sure you get stacked frames to pass into the policy
-#	make sure that you get the game to work with the policy
+import scipy.signal
+
+sess = tf.Session()
 
 # Taken straight from Nature paper from DeepMind
 def NatureCNN(images):
 	# we apply the basic Nature feature extractor
 	he_uniform = keras.initializers.he_uniform
-	conv1 = layers.conv2d(images, 32, 8, 4)
-	conv2 = layers.conv2d(conv1, 64, 4, 2)
-	conv3 = layers.conv2d(conv2, 64, 3, 1)
-	conv3 = layers.flatten(conv3)
-	hOut = slim.fully_connected(conv3, 512)
+	conv1_1 = layers.conv2d(images, 32, 8, stride=4)
+	conv2_1 = layers.conv2d(conv1_1, 64, 4, stride=2)
+	conv3_1 = layers.conv2d(conv2_1, 64, 3, stride=1)
+	conv3_1 = layers.flatten(conv3_1)
+	hidden_1 = layers.fully_connected(conv3_1, 512)
 	# return a 512 dimensional embedding
-	return hOut
+	return hidden_1
 
 def normalizedColumnsInitializer(std=1.0):
     def _initializer(shape, dtype=None, partition_info=None):
@@ -29,33 +34,33 @@ def normalizedColumnsInitializer(std=1.0):
         return tf.constant(out)
     return _initializer
 
-class LSTMPolicy():
-	def __init__(self, scope, reuse, inputDims, outputDims):
-		self.inputs = tf.placeholder(tf.float32, [None] + inputDims)
-		with tf.variable_scope(scope, reuse=reuse):
-			features = NatureCNN(self.inputs)
+# class LSTMPolicy():
+# 	def __init__(self, scope, reuse, inputDims, outputDims):
+# 		self.inputs = tf.placeholder(tf.float32, [None] + inputDims)
+# 		with tf.variable_scope(scope, reuse=reuse):
+# 			features = NatureCNN(self.inputs)
 
-			lstm = rnn.BasicLSTMCell(512, state_is_tuple=True)
+# 			lstm = rnn.BasicLSTMCell(512, state_is_tuple=True)
 
-			contextIn = tf.placeholder(tf.float32, [1, lstm.state_size.c])
-			hiddenIn = tf.placeholder(tf.float32, [1, lstm.state_size.h])
-			stateIn = rnn.LSTMStateTuple(contextIn, hiddenIn)
+# 			contextIn = tf.placeholder(tf.float32, [1, lstm.state_size.c])
+# 			hiddenIn = tf.placeholder(tf.float32, [1, lstm.state_size.h])
+# 			stateIn = rnn.LSTMStateTuple(contextIn, hiddenIn)
 
-			lstmOutputs, lstmState = nn.dynamic_rnn(lstm, features, 
-				initial_state=stateIn, sequence_length=)
+# 			lstmOutputs, lstmState = nn.dynamic_rnn(lstm, features, 
+# 				initial_state=stateIn, sequence_length=)
 
-			outputs = layers.flatten(lstmOutputs)
+# 			outputs = layers.flatten(lstmOutputs)
 
-			self.logits = slim.fully_connected(outputs, outputDims, 
-				weights_initializer=normalizedColumnsInitializer(0.01),
-                biases_initializer=None)
+# 			self.logits = slim.fully_connected(outputs, outputDims, 
+# 				weights_initializer=normalizedColumnsInitializer(0.01),
+#                 biases_initializer=None)
 
-			self.policy = nn.softmax(self.logits)
+# 			self.policy = nn.softmax(self.logits)
 
-			self.value = slim.fully_connected(outputs, 1, 
-				activation_fn=None,
-				weights_initializer=normalizedColumnsInitializer(1.0),
-                biases_initializer=None)
+# 			self.value = slim.fully_connected(outputs, 1, 
+# 				activation_fn=None,
+# 				weights_initializer=normalizedColumnsInitializer(1.0),
+#                 biases_initializer=None)
 
 class CNNPolicy():
 	def __init__(self, scope, reuse, inputDims, outputDims):
@@ -71,63 +76,94 @@ class CNNPolicy():
 				activation_fn=None,
 				weights_initializer=normalizedColumnsInitializer(1.0),
                 biases_initializer=None)
+			# we want self.value to have shape (None,) when it is still (None, 1) right now
+			self.value = tf.squeeze(self.value)
 
 class Actor():
 	def __init__(self, PolicyType, policyParams):
-		self.policyFunction = PolicyType(*policyParams)
+		self.numActions = policyParams['outputDims']
+		self.policyFunction = PolicyType(**policyParams)
 
 	def getActionsValues(self, state):
-		actionsDistribution, values = sess.run([
-			self.policyFunction.policy, self.policyFunction.value]
+		actionsDistribution, values = sess.run(
+			[self.policyFunction.policy, self.policyFunction.value],
 			feed_dict={
 				self.policyFunction.inputs: state
 			})
 		actionsDistribution = actionsDistribution[0]
-		actions = np.random.choice(actionsDistribution, p=actionsDistribution)
-		actions = np.argmax(actionsDistribution == actions)
+		actions = np.random.choice(list(range(self.numActions)), 
+			p=actionsDistribution)
+		# actions = np.argmax(actionsDistribution)
+		print(actionsDistribution, actions)
 		return actions, values
 
 class Worker():
-	def __init__(self, env, name, PolicyType, policyParams, gamma, PolicyOptimizer):
+	def __init__(self, env, name, PolicyType, policyParams, gamma, 
+		PolicyOptimizer, outputPath, maxGradNorm=0.5, history=4, batchSize=1000):
+		self.gamma = gamma
+		self.history = history
+		self.batchSize = batchSize
+		self.totalTransitions = 0
+		self.outputPath = outputPath
+		self.env = env
+		self.gamma = gamma
+		self.history = history
+		self.batchSize = batchSize
+		self.totalTransitions = 0
+		self.outputPath = outputPath
+		
 		self.actor = Actor(PolicyType, policyParams)
-		#self.swapPolicy = updatePolicy('global', self.name)
+		#remove#self.swapPolicy = updatePolicy('global', self.name)
 		self.replayBuffer = ReplayBuffer()
 		self.policyOptimizer = PolicyOptimizer(
-			self.actor, gamma, 'worker', 'global', 1e-4)
-		self.gamma = gamma
+			self.actor, gamma, 'worker', None, 5e-4, maxGradNorm)
+		self.addSummary()
 
-	def runPolicy():
+	def runPolicy(self):
+		sess.run(tf.global_variables_initializer())
+		sess.run(tf.local_variables_initializer())
 		with sess.as_default(), sess.graph.as_default():
-			while not coord.should_stop():
-				# first we need to update the worker policy with the
-				#	global policy
-				sess.run(self.swapPolicy)
-				# start a new episode
-				self.env.new_episode()
-				# we need to initialize our state representation
-				observation = self.env.get_state().screen_buffer
-				replayBuffer.addObservation(observation)
-				state = processFrame(observation)
-				# rollout the policy until either
-				# 	1. our replay buffer is full
-				#	2. the episode is done
-				# then train!
-				while not self.env.is_episode_finished():
-					actions, values = self.actor.getActionsValues(state)
-					nextObservations, rewards, dones, infos = self.env.step(actions)
-					nextState = processFrame(nextObservations)
-					replayBuffer.addTransition(state, action, reward, done, nextState, values)
-					state = nextState
-					# if our buffer is full
-					if self.replayBuffer.size >= self.batchSize:
-						# process rollouts first to calculate discounted rewards
-						bootstrap = None
-						if done:
-							_, bootstrap = self.actor.getActionsValues(state)
-						self.trainPolicy(bootstrapVal=bootstrap)
-			# train after rollout!
-			if self.replayBuffer.size > 0:
-				self.trainPolicy()
+			#while not coord.should_stop():
+			# first we need to update the worker policy with the
+			#	global policy
+			#remove#sess.run(self.swapPolicy)
+			# start a new episode
+			frame = self.env.reset()
+			# we need to initialize our state representation
+			frame = np.expand_dims(processFrame(frame), axis=0)
+			# we need to stick four frames together in the beginning
+			frameBuffer = [frame] * self.history
+			state = np.concatenate(frameBuffer, axis=3)
+			# rollout the policy until either
+			# 	1. our replay buffer is full
+			#	2. the episode is done
+			# then train!
+			while True:
+				self.totalTransitions += 1
+				action, value = self.actor.getActionsValues(state)
+				nextFrame, reward, done, info = self.env.step(action)
+
+				nextFrame = np.expand_dims(processFrame(nextFrame), axis=0)
+				frameBuffer = frameBuffer[:-1] + [nextFrame]
+				nextState = np.concatenate(frameBuffer, axis=3)
+				
+				self.replayBuffer.addTransition(state, action, reward, done, nextState, value)
+				state = nextState
+
+				self.env.render()
+				# if our buffer is full
+				if self.replayBuffer.size >= self.batchSize:
+					# process rollouts first to calculate discounted rewards
+					bootstrap = None
+					if done:
+						_, bootstrap = self.actor.getActionsValues(state)
+					self.trainPolicy(bootstrapVal=bootstrap)
+
+				if done:
+					break
+		# train after rollout!
+		if self.replayBuffer.size > 0:
+			self.trainPolicy()
 
 	def trainPolicy(self, bootstrapVal=None):
 		# handles getting the appropriate trajectories and 
@@ -135,19 +171,26 @@ class Worker():
 		self.replayBuffer.processRollouts(
 			bootstrapVal=bootstrapVal, gamma=self.gamma)
 		# perform training
-		sess.run([
-			self.optimizer.policyGradientLoss,
-			self.optimizer.valueFunctionLoss,
-			self.optimizer.entropy,
-			self.optimizer.train],
+		_, summary = sess.run([
+			self.policyOptimizer.train, self.merged],
 			feed_dict={
-				self.optimizer.actions: self.replayBuffer.actions,
-				self.optimizer.values: self.replayBuffer.discountedRewards,
-				self.optimizer.advantages: self.replayBuffer.advantages,
-				self.optimizer.actor.inputs: self.replayBuffer.states
+				self.policyOptimizer.actions: self.replayBuffer.actions,
+				self.policyOptimizer.values: self.replayBuffer.discountedRewards,
+				self.policyOptimizer.advantages: self.replayBuffer.advantages,
+				self.policyOptimizer.rewards: self.replayBuffer.rewards,
+				self.actor.policyFunction.inputs: self.replayBuffer.states
 			})
+		self.file_writer.add_summary(summary, self.totalTransitions)
 		# empty the replay buffer so we can store stuff in it again
 		self.replayBuffer.empty()
+
+	def addSummary(self):
+		tf.summary.scalar("Mean Reward", self.policyOptimizer.meanRewards)
+		tf.summary.scalar("PG Loss", self.policyOptimizer.policyGradientLoss)
+		tf.summary.scalar("VF Loss", self.policyOptimizer.valueFunctionLoss)
+
+		self.merged = tf.summary.merge_all()
+		self.file_writer = tf.summary.FileWriter(self.outputPath, sess.graph)
 
 def discount(x, gamma):
     return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
@@ -156,13 +199,17 @@ class ReplayBuffer():
 	def __init__(self):
 		self.empty()
 
-	def processRollouts(self, bootstrapVal=bootstrapVal, gamma=gamma):
+	def processRollouts(self, bootstrapVal=None, gamma=0.99):
 		# add bootstrap value to rewards
-		rewards_ = self.rewards + [bootstrapVal]
-		self.discountedRewards = discount(self.rewards_, gamma)[:-1]
-		values_ = self.values + [bootstrapVal]
-		self.advantages = rewards + gamma * self.values_[1:] - self.values_[:-1]
+		if bootstrapVal == None:
+			bootstrapVal = 0
+		rewards_ = np.array(self.rewards + [bootstrapVal])
+		self.discountedRewards = discount(rewards_, gamma)[:-1]
+		values_ = np.array(self.values + [bootstrapVal])
+		self.advantages = self.rewards + gamma * values_[1:] - values_[:-1]
 		self.advantages = discount(self.advantages, gamma)
+
+		self.states = np.squeeze(self.states, axis=1)
 
 	def addTransition(self, state, action, reward, done, nextState, value):
 		self.size += 1
@@ -170,7 +217,7 @@ class ReplayBuffer():
 		self.actions.append(action)
 		self.rewards.append(reward)
 		self.dones.append(done)
-		self.nextState.append(nextState)
+		self.nextStates.append(nextState)
 		self.values.append(value)
 
 	def empty(self):
@@ -183,12 +230,13 @@ class ReplayBuffer():
 		self.size = 0
 
 class GAEOptimizer():
-	def __init__(self, actor, gamma, scope, globalScope, lr):
+	def __init__(self, actor, gamma, scope, globalScope, lr, maxGradNorm):
 		self.actor = actor
 		self.gamma = gamma
 		self.scope = scope
 		self.globalScope = globalScope
 		self.lr = lr
+		self.maxGradNorm = maxGradNorm
 		self.policyGradient()
 		if self.globalScope != None:
 			self.updateGlobalVariables()
@@ -200,24 +248,27 @@ class GAEOptimizer():
 		self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
 		self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
 		self.values = tf.placeholder(shape=[None], dtype=tf.float32)
+		self.rewards = tf.placeholder(shape=[None], dtype=tf.float32)
+
+		self.meanRewards = tf.reduce_mean(self.rewards)
 
 		negativeLogLoss = nn.sparse_softmax_cross_entropy_with_logits(
-			logits=self.actor.logits, labels=self.actions)
+			logits=self.actor.policyFunction.logits, labels=self.actions)
 
 		self.policyGradientLoss = tf.reduce_mean(self.advantages * negativeLogLoss)
-		self.valueFunctionLoss = losses.mean_squared_error(self.values, self.actor.value)
+		self.valueFunctionLoss = losses.mean_squared_error(self.values, self.actor.policyFunction.value)
 
-		self.entropy = tf.reduce_mean(self.policy * tf.log(self.policy))
+		self.entropy = tf.reduce_mean(self.actor.policyFunction.policy * tf.log(self.actor.policyFunction.policy))
 		# the key is to drive vFLoss towards 0, pGLoss to a large negative number, 
 		# 	and entropy to 0 to minimize loss.
-		self.loss = valueLossCoeff * valueFunctionLoss + \
-					policyGradientLoss + \
-					entropyCoeff * entropy
-		variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
-		self.gradients = tf.gradients(loss, variables)
+		self.loss = valueLossCoeff * self.valueFunctionLoss + \
+					self.policyGradientLoss + \
+					entropyCoeff * self.entropy
+		variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+		self.gradients = tf.gradients(self.loss, variables)
 		self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
-		if maxGradNorm is not None:
-			self.gradients, norm = tf.clip_by_global_norm(gradients, maxGradNorm)
+		if self.maxGradNorm is not None:
+			self.gradients, norm = tf.clip_by_global_norm(self.gradients, self.maxGradNorm)
 
 	def updateGlobalVariables(self):
 		# performs a global update
@@ -225,11 +276,11 @@ class GAEOptimizer():
 		globalVariables = tf.get_collection(
 			tf.GraphKeys.TRAINABLE_VARIABLES, self.globalScope)
 		globalGradsAndVars = zip(self.gradients, globalVariables)
-		self.train = optimizer.apply_gradients(globalGradsAndVars)
+		self.train = self.optimizer.apply_gradients(globalGradsAndVars)
 
 	def updateLocalVariables(self):
 		# performs a local update
 		localVariables = tf.get_collection(
 			tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
 		gradsAndVars = zip(self.gradients, localVariables)
-		self.train = optimizer.apply_gradients(gradsAndVars)
+		self.train = self.optimizer.apply_gradients(gradsAndVars)
---
+++ run.py
@@ -0,0 +1,19 @@
+from practicea3c import Worker, Actor, CNNPolicy, GAEOptimizer
+
+import gym
+import tensorflow as tf
+
+env = gym.make('Pong-v0')
+
+policyParams = {
+	'scope': 'worker',
+	'reuse': tf.AUTO_REUSE,
+	'inputDims': [80, 80, 4],
+	'outputDims': env.action_space.n
+}
+
+worker = Worker(env, 'worker', CNNPolicy, policyParams, 0.99, GAEOptimizer, 'results/')
+
+while True:
+	if worker.totalTransitions < 5e6:
+		worker.runPolicy()
